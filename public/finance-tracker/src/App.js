c4ef298071cf0e6bc5633901cffe38082ef3dd9f
nicoledperez <ndperez@syr.edu>
2019-08-07 18:50:14 -0400

    Setting Up basic navbar
    - added react bootstrap
    - created basic nav bar with buttons for the overview, budget, and reports

--- public/finance-tracker/src/App.js
+++ public/finance-tracker/src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { tabIndex: 0 };
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+		<Navbar bg="light" expand="lg">
+		  <Navbar.Brand href="#home">Finance Tracker</Navbar.Brand>
+		  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+		  <Navbar.Collapse id="basic-navbar-nav">
+			<Nav className="mr-auto">
+			  <Nav.Link href="#home">Overview</Nav.Link>
+			  <Nav.Link href="#link">Budget</Nav.Link>
+			  <Nav.Link href="#reports">Reports</Nav.Link>
+			</Nav>
+		  </Navbar.Collapse>
+		</Navbar>
     );
   }
 }
