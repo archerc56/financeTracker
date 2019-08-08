7a9e9dba5cb19668b6eae781b8e8613da9799928
nicoledperez <ndperez@syr.edu>
2019-08-07 22:03:30 -0400

    Adding basics of navbar
    - added react bootstrap
    - added navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { tabIndex: 0 };
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to FinanceTool</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+		<Navbar bg="light" expand="lg">
+		  <Navbar.Brand href="#overview">Finance Tracker</Navbar.Brand>
+		  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+		  <Navbar.Collapse id="basic-navbar-nav">
+			<Nav className="mr-auto">
+			  <Nav.Link href="#overview">Overview</Nav.Link>
+			  <Nav.Link href="#link">Budget</Nav.Link>
+			  <Nav.Link href="#reports">Reports</Nav.Link>
+			</Nav>
+		  </Navbar.Collapse>
+		</Navbar>
     );
   }
 }
