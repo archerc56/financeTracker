44085b3ea8c9c78c22518231ecb0049c0bbc5eea
nicoledperez <ndperez@syr.edu>
2019-08-22 21:41:38 -0400

    Adding Goals page
    - added goals page

---
+++ src/Goals/Goals.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class Goals extends Component {
+  constructor() {
+    super();
+    this.state = {  };
+  }
+  render() {
+    return (
+		<p> These are the goals.</p>
+    );
+  }
+}
+
+export default Goals;
+
+
+
