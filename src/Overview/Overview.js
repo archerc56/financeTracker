7aa1ecfbc9c8ac4a82e96455e0c9a8557384c215
nicoledperez <ndperez@syr.edu>
2019-08-26 21:51:37 -0400

    Reformatting Overview Page
    - invisible reformat of overview page to move data into componentDidMount function to allow for loading fromt the database when it's time

--- src/Overview/Overview.js
+++ src/Overview/Overview.js
@@ -12,43 +12,79 @@ import '../App.css';
 class Overview extends Component {
   constructor() {
     super();
-    this.state = {  };
+    this.state = { 
+		accounts: [],
+	};
+  }
+  
+  componentDidMount() {
+	  //this will be where we do the api call to load the data but for now we will set the state here
+	  this.setState({
+		  accounts: [ 
+			{
+				name:'Checking Account', 
+				id: '1', 
+				transactions: [
+					{date: '1/3/2019', description: 'Snacks', category: 'Food', cost: '10.00'},
+					{date: '1/2/2019', description: 'Electric', category: 'Utilities', cost: '40.00'},
+					{date: '1/1/2019', description: 'Rent', category: 'Housing', cost: '1000.00'},
+				]
+			},
+			{
+				name:'Savings Account', 
+				id: '2', 
+				transactions: [
+					{date: '1/7/2019', description: 'Interest', category: 'Income', cost: '10.00'},
+					{date: '1/6/2019', description: 'Paycheck', category: 'Income', cost: '4000.00'},
+				]
+			},
+		],
+	  });
   }
   
   
-  
   render() {
+	
+	const mapAccount = (account) => {
+		return (
+			<ListGroup.Item action href={`#link${account.id}`} >
+				{account.name}
+			</ListGroup.Item>
+		);
+	};
+	
+	const mapTransactions = (account) => {
+		return (
+			<Tab.Pane eventKey={`#link${account.id}`}>
+				<AccountsTable
+					transactions={account.transactions}
+				/>
+			</Tab.Pane>
+		);
+	};
+	
     return (
 	<div className={'wrapper--large'}>
-	
+		<ButtonToolbar className={'wrapper--small'}>
+			<Button variant="dark" size="lg">Add Account</Button>
+		</ButtonToolbar>
 		<Tab.Container id="list-group-tabs-example" defaultActiveKey="#link1">
 		  <Row>
 			<Col sm={2}>
 			  <ListGroup>
-				<ListGroup.Item action href="#link1" >
-				  Checking Account
-				</ListGroup.Item>
-				<ListGroup.Item action href="#link2">
-				  Savings Account
-				</ListGroup.Item>
+				{ this.state.accounts.map(mapAccount) }
 			  </ListGroup>
 			</Col>
 			<Col sm={10}>
 			  <Tab.Content>
-				<Tab.Pane eventKey="#link1">
-				  <AccountsTable/>
-				</Tab.Pane>
-				<Tab.Pane eventKey="#link2">
-				  Some more stuff for Link 2 page
-				</Tab.Pane>
+			  <ButtonToolbar className={'wrapper--small'}>
+				  <Button variant="dark" size="lg">Add Transaction</Button>
+			  </ButtonToolbar>
+				{ this.state.accounts.map(mapTransactions) }
 			  </Tab.Content>
 			</Col>
 		  </Row>
 		</Tab.Container>
-		<ButtonToolbar className={'wrapper--small'}>
-			<Button variant="success" size="lg">Add Account</Button>
-		</ButtonToolbar>
-		
 		</div>
     );
   }
